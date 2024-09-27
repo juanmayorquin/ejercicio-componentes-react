708a987bfabbb403e4b86e7eeabd2d9d921c7031
juanmayorquin <juanjmayor@outlook.es>
2024-09-26 21:08:39 -0500

    Ejercicio finalizado

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Cards from "./assets/componentes/Cards";
+import "./App.css";
+import VideoCard from "./assets/componentes/videoCard/VideoCard";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Cards />
+      <VideoCard link={"https://www.youtube.com/embed/dQw4w9WgXcQ?si=TjBr4R45Y5RSCRUN"}/>
+      <h1>Vite + React</h1>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
